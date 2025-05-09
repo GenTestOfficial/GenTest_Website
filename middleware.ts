ec0099ed321cfc05a9ba1b3635904fc6dfc4446d
Radu Mihai Galan <radumgalan@gmail.com>
2025-05-09 17:19:34 +0300

    sitemap created

--- middleware.ts
+++ middleware.ts
@@ -10,14 +10,18 @@ export default clerkMiddleware((auth, req) => {
   if (isPublic(req)) {
     const response = NextResponse.next()
     response.headers.set('X-Robots-Tag', 'index, follow')
+    response.headers.set('Cache-Control', 'public, max-age=3600')
     return response
   }
-  return NextResponse.next()
+  
+  const response = NextResponse.next()
+  response.headers.set('X-Robots-Tag', 'noindex, nofollow')
+  return response
 })
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and all static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
